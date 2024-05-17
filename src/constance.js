d1693eb149917c55feaeb65859d1e949d4cf0c71
geekyogeshcode <officialyogesh7218@gmail.com>
2024-05-17 21:57:06 +0530

    Constant updated

--- src/constance.js
+++ src/constance.js
@@ -1,20 +1,20 @@
 export const LANGUAGE_VERSIONS = {
-    javascript: '15.3.0',  // Update with a valid version
-    typescript: '5.1',
-    python: '3.11.3',
-    java: '20',
-    csharp: '12',
-    php: '8.2'
+    javascript: '18.15.0',  // Update with a valid version
+    typescript: '5.0.3',
+    python: '3.10.0',
+    java: '15.0.2',
+    csharp: '6.12.0',
+    php: '8.2.3'
   };
   
 export const CODE_SNIPPETS={
     javascript:`console.log("Hello, Javascript!");`,
     typescript:`console.log("Hello, Typescript!");`,
-    python:`print("Hello, World!")
+    python:`print("Hello, World! python")
     `,
     java:`public class HelloWorld {
         public static void main(String[] args) {
-            System.out.println("Hello, World!");
+            System.out.println("Hello, World! java");
         }
 }
     `,
@@ -22,12 +22,12 @@ export const CODE_SNIPPETS={
 
     class Program {
         static void Main() {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World! c#");
         }
 }
     `,
     php:`<?php
-    echo "Hello, World!";
+    echo "Hello, World! php";
 ?>
     `,
     Solidity:`// SPDX-License-Identifier: MIT
